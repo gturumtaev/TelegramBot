41d7cc7cc66e3b012f6e298534913ed697055444
gerchikk <geraloi@yandex.ru>
2023-11-23 16:13:07 +0500

    Дополнил метод process -> теперь обрабатывает сообщение от пользователя

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -4,21 +4,32 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.entity.Notification;
+import pro.sky.telegrambot.repository.NotificationRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private static final Pattern PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
+
+    @Autowired
+    private NotificationRepository notificationRepository;
 
     @PostConstruct
     public void init() {
@@ -29,10 +40,31 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            if (update.message().text().equals("/start") && update.message().text() != null) {
-                System.out.println("Приветствую в моем боте!");
+            String newMessage = update.message().text();
+            Long chatId = update.message().chat().id();
+            if (newMessage != null && newMessage.equals("/start")) {
+                String welcomeMessage = "Приветствую! Отправь сообщение в формате 01.01.2022 20:00 Сделать домашнюю работу";
+                SendMessage sendMessage = new SendMessage(chatId, welcomeMessage);
+                telegramBot.execute(sendMessage);
             } else {
+                Matcher matcher = PATTERN.matcher(newMessage);
+                if (matcher.matches()) {
+                    String dataTime = matcher.group(1);
+                    String notificationText = matcher.group(3);
 
+                    Notification notification = new Notification();
+                    notification.setNotificationText(notificationText);
+                    notification.setDateTime(LocalDateTime.parse(dataTime, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+                    notification.setChatId(chatId.intValue());
+                    notificationRepository.save(notification);
+                    String botAnswerTrue = "Напоминание будет отправлено в назначенное время";
+                    SendMessage sendMessage = new SendMessage(chatId, botAnswerTrue);
+                    telegramBot.execute(sendMessage);
+                }else {
+                    String botAnswerFalse = "Сообщение направлено не в верном формате! Отправьте сообщение в соответствии с заданным форматом";
+                    SendMessage sendMessage = new SendMessage(chatId, botAnswerFalse);
+                    telegramBot.execute(sendMessage);
+                }
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
---
+++ src/main/java/pro/sky/telegrambot/timer/ScheduledNotification.java
@@ -0,0 +1,9 @@
+package pro.sky.telegrambot.timer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledNotification {
+
+
+}
